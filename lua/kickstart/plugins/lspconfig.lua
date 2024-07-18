2e4b0a655e40bff0cac21e3f14e4cbfdbf6caa59
Darun Seethammagari <darunrs@gmail.com>
2024-07-18 10:00:59 -0700

    Use eslint LSP instead

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -161,7 +161,16 @@ return {
         -- pyright = {},
         rust_analyzer = {},
         graphql = {},
-        tsserver = {},
+        eslint = {
+          settings = {
+            eslint = {
+              autoFixOnSave = true,
+              format = {
+                enable = true,
+              },
+            },
+          },
+        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
