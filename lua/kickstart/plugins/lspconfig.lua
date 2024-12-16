fce7bd57f80b5e0b47a3e15830cca1ed3a350c03
Darun Seethammagari <darunrs@gmail.com>
2024-12-16 12:55:29 -0800

    replace tsserver

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -182,7 +182,7 @@ return {
         -- pyright = {},
         rust_analyzer = {},
         graphql = {},
-        tsserver = {},
+        ts_ls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
